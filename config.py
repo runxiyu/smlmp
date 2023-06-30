d92e2face329cef6911503b3f308a28ac82df728
Andrew Yu <andrew@andrewyu.org>
2023-06-30 20:12:20 +0000

    Change db.json location for new system user

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ RECIPIENT_DELIMITER = "+"
 POSTMASTER = "andrew@andrewyu.org"
 SMTPHOST = "localhost"
 SMTPPORT = 0  # use smtplib default
-PATH_TO_DBJSON = "/home/test/smlmp/db.json"
+PATH_TO_DBJSON = "/srv/smlmp/smlmp/db.json"
 HTTP_ROOT = "https://lists.andrewyu.org/"
